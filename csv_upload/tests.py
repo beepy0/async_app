bbdd2a23d710f7f1c2a5c2364e315061b5d3c506
Branimir-Ubuntu-Home <bpavlov@dentolo.de>
2020-06-07 19:23:34 +0200

    Added tests for the async task

--- csv_upload/tests.py
+++ csv_upload/tests.py
@@ -2,6 +2,9 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.messages import get_messages
 
+from .tasks import push_data_to_db
+from .models import User
+
 
 # Create your tests here.
 class TestUploadView(TestCase):
@@ -33,3 +36,27 @@ class TestUploadView(TestCase):
             response = self.client.post('/upload/', {'name': 'invalid.csv', 'file': fp})
             self.assertEqual(response.status_code, 200)
             self.assertTemplateUsed(response, 'csv_upload/upload_success.html')
+
+
+class TestAsyncTask(TestCase):
+    def test_skip_empty_mail(self):
+        with open('csv_upload/test_data/data_example_empty_mail.csv', 'rb') as fp:
+            data = fp.read().decode('UTF-8')
+            db_users_cnt_before = len(User.objects.all())
+            push_data_to_db(data)
+            self.assertEqual(db_users_cnt_before, len(User.objects.all()))
+
+    def test_adding_new_users(self):
+        with open('csv_upload/test_data/data_example.csv', 'rb') as fp:
+            data = fp.read().decode('UTF-8')
+            db_users_cnt_before = len(User.objects.all())
+            push_data_to_db(data)
+            self.assertTrue(db_users_cnt_before + 6 == len(User.objects.all()))
+
+    def test_adding_duplicate_users(self):
+        with open('csv_upload/test_data/data_example.csv', 'rb') as fp:
+            data = fp.read().decode('UTF-8')
+            db_users_cnt_before = len(User.objects.all())
+            push_data_to_db(data)
+            push_data_to_db(data)
+            self.assertTrue(db_users_cnt_before + 6 == len(User.objects.all()))
